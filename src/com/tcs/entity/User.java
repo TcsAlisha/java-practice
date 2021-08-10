ab6e2b3559c2ba9e8bece4978b7f54c85d10f7f4
TcsAlisha <tcsalishadias@gmail.com>
2021-08-10 10:23:53 +0000

    new changes

--- src/com/tcs/entity/User.java
+++ src/com/tcs/entity/User.java
@@ -3,60 +3,52 @@ package com.tcs.entity;
 public class User {
 	private String name;
 	private Long mobilenumber;
-	private byte age;
 	private Role role;
-
+	private byte age;   //data encapsulation
+	
+	//constructor
 	public void workout() {
 		System.out.println(name + "is working out..");
-
 	}
 	
-	public User(String name, Long mobilenumber, byte age) {
-		
-		this(name,mobilenumber);
+	//custom constructor
+	public User(String name, int i, byte age) {
+		this.name = name;
+		this.mobilenumber = mobilenumber;
 		this.age = age;
 	}
 	
-//	public User(String name, Long mobilenumber) {
-//		super();
-//		this.name = name;
-//		this.mobilenumber = mobilenumber;
-//	}
+	//overloaded constructor
+	public User(String name, Long mobilenumber) {
+		super(); //call super class constructor
+		this.name = name;
+		this.mobilenumber = mobilenumber;
+		
+	}
 
 	public String getName() {
 		return name;
 	}
-
-	public User(String name, Long mobilenumber) {
-		super();
-		this.name = name;
-		this.mobilenumber = mobilenumber;
-	}
-
+	
 	public void setName(String name) {
 		this.name = name;
 	}
-
 	public Long getMobilenumber() {
 		return mobilenumber;
 	}
-
 	public void setMobilenumber(Long mobilenumber) {
 		this.mobilenumber = mobilenumber;
 	}
-
 	public byte getAge() {
 		return age;
 	}
-
 	public void setAge(byte age) {
 		this.age = age;
 	}
-
+	
 	@Override
 	public String toString() {
-
-		return "[name=" + name + "]";
+		return "[name=" +name+"]";
 	}
 
 }
