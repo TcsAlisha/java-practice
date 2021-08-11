035a7fbac2eb7d2bfe95206a635e7b88d4ad8a0c
TcsAlisha <tcsalishadias@gmail.com>
2021-08-11 10:52:19 +0000

    multithreading

--- src/com/tcs/errordemo/ErrorDemo.java
+++ src/com/tcs/errordemo/ErrorDemo.java
@@ -1,17 +1,17 @@
 package com.tcs.errordemo;
 
-//public class ErrorDemo {
-//	public static void main(String[] args) {
-//		new ErrorDemo().infiniteMethod();
-//	}
-//	private void infiniteMethod() {
-//		infiniteMethod();
-//	}
-//}
 public class ErrorDemo {
-	  public static void main (String[] args) {
-	    Object error = new Error();
-	    Object runtimeException = new RuntimeException();
-	    System.out.print((error instanceof Exception) + ",");
-	    System.out.print(runtimeException instanceof Exception);
-	}}
+	public static void main(String[] args) {
+		new ErrorDemo().infiniteMethod();
+	}
+	private void infiniteMethod() {
+		infiniteMethod();
+	}
+}
+//public class ErrorDemo {
+//	  public static void main (String[] args) {
+//	    Object error = new Error();
+//	    Object runtimeException = new RuntimeException();
+//	    System.out.print((error instanceof Exception) + ",");
+//	    System.out.print(runtimeException instanceof Exception);
+//	}}
---
+++ src/com/tcs/threads/Manager.java
@@ -0,0 +1,15 @@
+package com.tcs.threads;
+
+import java.util.Iterator;
+
+public class Manager {
+	public static void main(String[] args) {
+		
+		for (int i = 0; i < 10; i++) {
+			Worker worker1 = new Worker();
+			Thread t1 =  new Thread(worker1);
+			t1.start();
+			System.out.println(Thread.currentThread().toString()+i);
+		}
+	}
+}
