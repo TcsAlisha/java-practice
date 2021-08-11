c185a0c1bbd55f4de05149d5224aabcd5689f2fb
TcsAlisha <tcsalishadias@gmail.com>
2021-08-11 05:43:01 +0000

    abstraction, final

---
+++ src/com/tcs/final1/keyword/Ticket.java
@@ -0,0 +1,13 @@
+package com.tcs.final1.keyword;
+
+public final class Ticket {
+	private final String movie="Sholay";
+	private float price;
+	
+	public final void sell() {
+		
+	}
+	public final void sell(float price) {
+		
+	}
+}
