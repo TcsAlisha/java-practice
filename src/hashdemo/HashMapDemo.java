b6312753c322ca9e38f2e3978503bf53a1989565
TcsAlisha <tcsalishadias@gmail.com>
2021-08-12 08:56:48 +0000

    TreeMap

--- src/hashdemo/HashMapDemo.java
+++ src/hashdemo/HashMapDemo.java
@@ -2,10 +2,20 @@ package hashdemo;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class HashMapDemo {
 	public static void main(String[] args) {
-		createHashMap();
+		//createHashMap();
+		treeMap();
+	}
+
+	private static void treeMap() {
+		Map<Integer,Float> mathScores= new TreeMap<Integer,Float>();
+		for (int i = 0; i < 20; i++) {
+			mathScores.put( i,new Float(100 * Math.random()));
+		}
+		System.out.println(mathScores);
 	}
 
 	private static void createHashMap() {
@@ -13,6 +23,10 @@ public class HashMapDemo {
 		for (int i = 0; i < 20; i++) {
 			mathScores.put("Student"+ i,new Float(100 * Math.random()));
 		}
+		boolean exists = mathScores.containsKey("Student1");
+		System.out.println(exists);
+		mathScores.put("Student1", 34F);
+		mathScores.remove("Student1");
 		System.out.println(mathScores);
 	}
 }
